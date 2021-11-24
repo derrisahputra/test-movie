7d2c751eafabc770d50f4d1285f28cbf18d10e3d
Derri Sahputra <derrisahputra@gmail.com>
2021-11-24 07:22:14 +0700

    Adding require dotenv

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const serveStatic = require('serve-static');
 const path = require('path');
-
 const app = express();
+require('dotenv').config()
 
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
@@ -10,6 +10,6 @@ app.get(/.*/, function (req, res) {
     res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
 
-const port = process.env.VUE_APP_PORT_SERVER;
+const port = process.env.VUE_APP_PORT_SERVER || 8000;
 app.listen(port);
 console.log(`App is listening on port: ${port}`);
